19d2292a2ee10fc7f3eae4a332b9fb4beee7ba3c
Pornchai <63188163+biggujp@users.noreply.github.com>
2026-08-02 16:13:55 +0700

    update

--- coffee-pwa1/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <header className="border-b bg-white px-6 py-4">
-      <h1 className="text-xl font-bold">
-        Coffee Admin
-      </h1>
-    </header>
-  );
-}
